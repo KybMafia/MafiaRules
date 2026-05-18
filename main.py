fed9caf049c605412b74bf1413f44fb458c773ac
white <whitezelendewagen@mail.ru>
2026-05-18 15:19:50 +1000

    update

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def create_pdf(name):
     pdf.add_font('CustomFont', '', font_path, uni=True)
 
     yellow = (255, 255, 0)
-    red = (235, 79, 58)
-    green = (140, 233, 110)
+    red = (255, 79, 58)
+    green = (140, 255, 0)
     blue = (0, 111, 255)
     orange = (255, 155, 0)
     white = (255, 255, 255)
@@ -42,10 +42,8 @@ def create_pdf(name):
     pdf.set_left_margin(10)
     pdf.set_right_margin(10)
 
-    line_height = 8
-
     for title, text in data:
-        needed_height = line_height * (1.5 + 3)  # заголовок 1.5 линии + 3 строки текста
+        needed_height = 8 * (1.5 + 13)  # размер пустоты для новой страницы
         current_y = pdf.get_y()
         if current_y + needed_height > pdf.page_break_trigger:
             pdf.add_page()
@@ -62,15 +60,40 @@ def create_pdf(name):
         elif title in ['Золотая минута', 'Переголосование', 'Вскрытие роли']:
             pdf.set_text_color(*yellow)
         elif title in ['Что нового:', 'Рейтинг']:
-            pdf.set_text_color(*orange)
+            pdf.set_text_color(*green)
         else:
             pdf.set_text_color(*green)
-        pdf.set_font('CustomFont', size=28)
-        pdf.cell(0, line_height * 2, title, ln=True, align="C")
 
+        # ─── Заголовок ────────────────────────────────────────────────────────────
+        pdf.set_font('CustomFont', size=28)
+        pdf.multi_cell(0, 16, title, align="C")
+
+        # ─── Картинка ────────────────────────────────────────────────────────────
+        images = False
+        if images:
+            titles_names = {'Главное меню': '1',
+                            'Управление игрой': '2',
+                            'Режимы раздачи ролей': '4',
+                            'Окно знакомства': '6',
+                            'Окно ночного выбора': '7',
+                            'Окно результата ночи': '9',
+                            'Окно результата голосования': '10',
+                            'Отмена результата ночи или голосования': '11',
+                            'Рейтинговая система': '12'}
+            if title in titles_names:
+                img_path = f'images/{titles_names[title]}.jpg'
+                text_width = pdf.w - pdf.l_margin - pdf.r_margin
+                pdf.image(img_path, x=pdf.l_margin, w=text_width)
+                pdf.ln(5)
+                if titles_names[title] in ['2', '4', '7']:
+                    img_path = f'images/{int(titles_names[title]) + 1}.jpg'
+                    text_width = pdf.w - pdf.l_margin - pdf.r_margin
+                    pdf.image(img_path, x=pdf.l_margin, w=text_width)
+                    pdf.ln(5)
+        # ─── Текст ────────────────────────────────────────────────────────────────
         pdf.set_text_color(*white)
         pdf.set_font('CustomFont', size=22)
-        pdf.multi_cell(0, line_height, text.strip())
+        pdf.multi_cell(0, 9, text.strip(), align="L")
 
     pdf.output('ПРАВИЛА' + '.pdf')
     print(f"PDF создан: {name}")
